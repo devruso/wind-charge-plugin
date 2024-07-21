c443f6263596bba62b880cecb9d446112df5e533
Jamilson Pestana Jr <jamilsonjr@live.com>
2024-07-21 16:44:51 -0300

    feature/wip: base plugin class w/ functions to enable and disable

---
+++ windCharge/windCharge/src/com/devruso/windCharge/WindCharge.java
@@ -0,0 +1,17 @@
+package com.devruso.windCharge;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class WindCharge extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+        System.out.println("Hey");
+    }
+
+    @Override
+    public void onDisable(){
+        System.out.println("Hey");
+    }
+
+}
